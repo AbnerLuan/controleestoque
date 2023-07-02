15e5867426bbaa4645bd3f8ef6479542a7f1a1d1
Abner Luan <abnerldsantosgmail.com>
2023-07-02 12:26:45 -0300

    ajuste no prodconfig

--- src/main/java/com/luan/controleestoque/config/ProdConfig.java
+++ src/main/java/com/luan/controleestoque/config/ProdConfig.java
@@ -1,28 +1,28 @@
-package com.luan.controleestoque.config;
-
-import com.luan.controleestoque.service.DBService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-@Configuration
-@Profile("prod")
-public class ProdConfig {
-
-    @Autowired
-    private DBService dbService;
-
-    @Value("${spring.jpa.hibernate.ddl-auto}")
-    private String value;
-
-    @Bean
-    public boolean instanciaDB() {
-        if (value.equals("create")) {
-            this.dbService.instanciaDB();
-        }
-        return false;
-    }
-
-}
+//package com.luan.controleestoque.config;
+//
+//import com.luan.controleestoque.service.DBService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//
+//@Configuration
+//@Profile("prod")
+//public class ProdConfig {
+//
+//    @Autowired
+//    private DBService dbService;
+//
+//    @Value("${spring.jpa.hibernate.ddl-auto}")
+//    private String value;
+//
+//    @Bean
+//    public boolean instanciaDB() {
+//        if (value.equals("create")) {
+//            this.dbService.instanciaDB();
+//        }
+//        return false;
+//    }
+//
+//}
